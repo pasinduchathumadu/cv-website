2b9f3ac6709b9530022985f14a5758274404fb34
pasindu <pasindugura123@gmail.com>
2023-07-25 01:08:25 +0530

    changed the files

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -34,12 +34,12 @@ const Portfolio = () => {
       ];
     return(
         <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full text-white ">
-            <div className="max-w-screen p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
                 <div className="pb-8">
-                    <p className="text-4xl font-bold inline border-b-4 border-gray-500 ml-14">Portfolio</p>
-                    <p className="py-6 ml-14">Check Out some of my work right here</p>
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">Portfolio</p>
+                    <p className="py-6 ">Check Out some of my work right here</p>
                 </div>
-                <div  className="grid sm:grid-cols-3 md:grid-cols-3 gap-8 px-12 sm:px-0">
+                <div  className="grid sm:grid-cols-2 md:grid-cols-2 gap-8 px-12 sm:px-0">
                 {portfolios.map(({id,src})=>(
                    
                    <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
