2148d52295788eaa716feb25470ce2b273774c93
pasindu <pasindugura123@gmail.com>
2023-07-28 08:41:09 +0530

    I added

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -35,21 +35,32 @@ const Portfolio = () => {
   ];
 
   return (
-    <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full text-white">
-      <div className="max-w-screen-lg p-1 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">Projects</p>
-          <p className="py-6">Check Out some of my work right here</p>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-8 px-1 sm:px-0">
-          {portfolios.map(({ id, src }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-              <img src={src} alt="" className="h-64 w-full object-cover rounded-md duration-300 hover:scale-105" />
-            </div>
-          ))}
-        </div>
+    <div
+    name="portfolio"
+    className="bg-gradient-to-b from-black to-gray-800 w-full md:h-screen text-white"
+  >
+    <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="pb-8">
+        <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+          Portfolio
+        </p>
+        <p className="py-6">Check out some of my work right here</p>
+      </div>
+
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+        {portfolios.map(({ id, src }) => (
+          <div key={id} className=" shadow-md shadow-gray-600 rounded-lg">
+            <img
+              src={src}
+              alt=""
+              className="rounded-md duration-200 hover:scale-105"
+            />
+          
+          </div>
+        ))}
       </div>
     </div>
+  </div>
   );
 }
 
