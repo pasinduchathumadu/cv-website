1f402e66c3c63d3c92d84065b109b74388e98377
pasindu <pasindugura123@gmail.com>
2023-09-15 12:40:05 +0530

    added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,7 +3,7 @@ import Home from './components/Home';
 import SocialLinks from './components/SocialLinks';
 import './App.css';
 import About from './components/About';
-import Portfolio from './components/Portfolio';
+import Portfolio from './components/project';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
